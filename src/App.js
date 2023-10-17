e7317937002768e888e5743c0eff8c482f819e45
Lianne-G <127854048+Lianne-G@users.noreply.github.com>
2023-10-17 17:24:23 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Navigate,Routes, Route } from "react-router-dom";
 import Home from "../src/pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+     <Route path="/" element={<Navigate to="/home"/>} />
         <Route path="/home" element=<Home /> />
       </Routes>
     </div>
